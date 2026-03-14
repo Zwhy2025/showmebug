4a3d54b337ebb54d459e92d6c64b44c202f523e9
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:44 +0800

    start n-queens

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: N 皇后" << '\n';
+    std::cout << "Slug: n-queens" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
