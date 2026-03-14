720422816219647291c86f910c7ee2cd6a035ebf
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:49 +0800

    start kth-largest-element-in-an-array

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 数组中的第K个最大元素" << '\n';
+    std::cout << "Slug: kth-largest-element-in-an-array" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
