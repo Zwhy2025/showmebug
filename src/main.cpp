a8f9b43a94764119e80e005ef1042c8624b52b2c
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:49 +0800

    start largest-rectangle-in-histogram

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 柱状图中最大的矩形" << '\n';
+    std::cout << "Slug: largest-rectangle-in-histogram" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
