2ab37289cf5867943431c7632829ee7a45905bd6
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:36 +0800

    start convert-sorted-array-to-binary-search-tree

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 将有序数组转换为二叉搜索树" << '\n';
+    std::cout << "Slug: convert-sorted-array-to-binary-search-tree" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
