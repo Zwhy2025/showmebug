90067b5fe7cc903cffe8c265695c71953e70778e
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:47 +0800

    start median-of-two-sorted-arrays

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 寻找两个正序数组的中位数" << '\n';
+    std::cout << "Slug: median-of-two-sorted-arrays" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
