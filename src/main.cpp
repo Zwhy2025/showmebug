08a371d82edf01e5ac13685d894a207c0bdcee6d
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:27 +0800

    start intersection-of-two-linked-lists

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 相交链表" << '\n';
+    std::cout << "Slug: intersection-of-two-linked-lists" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
