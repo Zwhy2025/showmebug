5749cb8a5d5f8f8cfa2efb1c0af033642d6f0143
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:48 +0800

    start daily-temperatures

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 每日温度" << '\n';
+    std::cout << "Slug: daily-temperatures" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
