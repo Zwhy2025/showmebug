91a2ca374e9966cb317a7a8900fd3deae194638e
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:19 +0800

    start container-with-most-water

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 盛最多水的容器" << '\n';
+    std::cout << "Slug: container-with-most-water" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
