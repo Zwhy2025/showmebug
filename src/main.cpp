7733eb8b6f7712f9c2d8666ee5363d79c8a8728d
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:44 +0800

    start palindrome-partitioning

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 分割回文串" << '\n';
+    std::cout << "Slug: palindrome-partitioning" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
