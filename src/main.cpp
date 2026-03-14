0feb4048f6d35201c3d7d5b6f435b34e781631e1
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:50 +0800

    start top-k-frequent-elements

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 前 K 个高频元素" << '\n';
+    std::cout << "Slug: top-k-frequent-elements" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
