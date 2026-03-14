b7c8298e45e2c7655b09bb72a8413894fe8cfd5a
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:29 +0800

    start merge-two-sorted-lists

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 合并两个有序链表" << '\n';
+    std::cout << "Slug: merge-two-sorted-lists" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
