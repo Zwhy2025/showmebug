1bdd5ba64a167aa36cd74a9523c67845ad434724
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:46 +0800

    start search-in-rotated-sorted-array

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 搜索旋转排序数组" << '\n';
+    std::cout << "Slug: search-in-rotated-sorted-array" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
