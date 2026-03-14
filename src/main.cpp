2f13475d3cd519c9b8e19ecdb7218562fdf2dbcc
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:43 +0800

    start generate-parentheses

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 括号生成" << '\n';
+    std::cout << "Slug: generate-parentheses" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
