3d0f0e63d6a25771dfa303f23c49c61dbb5df979
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:51 +0800

    start jump-game-ii

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 跳跃游戏 II" << '\n';
+    std::cout << "Slug: jump-game-ii" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
