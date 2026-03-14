33df942529029c25431a2160394c60627d21e5c6
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:39 +0800

    start path-sum-iii

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 路径总和 III" << '\n';
+    std::cout << "Slug: path-sum-iii" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
