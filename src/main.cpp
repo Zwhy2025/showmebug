3debf01f806e67ff0a588c8d99011959ba5f7758
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:40 +0800

    start course-schedule

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 课程表" << '\n';
+    std::cout << "Slug: course-schedule" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
--- src/solution.hpp
+++ src/solution.hpp
@@ -1,36 +1,31 @@
 #pragma once
 
 #include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <optional>
 #include <queue>
+#include <set>
 #include <stack>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *nextNode) : val(x), next(nextNode) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *leftNode, TreeNode *rightNode)
-        : val(x), left(leftNode), right(rightNode) {}
-};
+using namespace std;
 
 class Solution {
-  public:
-    // TODO: paste the current LeetCode method signature here.
-    // Example:
-    // int search(std::vector<int> &nums, int target) { return -1; }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+    }
 };
