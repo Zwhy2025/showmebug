9bae54f2e91a7ca7f4d7367cc0fc153e8f845d9f
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:45 +0800

    start search-insert-position

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 搜索插入位置" << '\n';
+    std::cout << "Slug: search-insert-position" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
