1f14b3f75f5b5bb30ca2b3d47f2f74402585702f
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:37 +0800

    start kth-smallest-element-in-a-bst

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 二叉搜索树中第 K 小的元素" << '\n';
+    std::cout << "Slug: kth-smallest-element-in-a-bst" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
