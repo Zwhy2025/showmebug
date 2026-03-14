6d7108271ce84209dc069d82647a273c00d1b1bd
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:32 +0800

    start merge-k-sorted-lists

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 合并 K 个升序链表" << '\n';
+    std::cout << "Slug: merge-k-sorted-lists" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
