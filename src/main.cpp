2883ae6aa3bd56ef0b2779576ba26acb185def87
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:50 +0800

    start find-median-from-data-stream

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 数据流的中位数" << '\n';
+    std::cout << "Slug: find-median-from-data-stream" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
