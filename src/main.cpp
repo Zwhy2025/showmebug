40c472e37087b753a60256f8412fea47ea37c77e
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:38 +0800

    start construct-binary-tree-from-preorder-and-inorder-traversal

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 从前序与中序遍历序列构造二叉树" << '\n';
+    std::cout << "Slug: construct-binary-tree-from-preorder-and-inorder-traversal" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
