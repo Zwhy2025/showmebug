46be81f3d2cddc34e16541add09ad11283d46a99
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:59 +0800

    start edit-distance

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 编辑距离" << '\n';
+    std::cout << "Slug: edit-distance" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
