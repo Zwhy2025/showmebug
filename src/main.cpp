e2918f5bc9ae0c171cc4b7e7b7153d3aa721451a
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:11:01 +0800

    start sort-colors

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 颜色分类" << '\n';
+    std::cout << "Slug: sort-colors" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
