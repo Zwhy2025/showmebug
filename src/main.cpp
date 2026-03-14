24f2a2d178298d8786658e70cb7be646eceab618
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:17 +0800

    start group-anagrams

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 字母异位词分组" << '\n';
+    std::cout << "Slug: group-anagrams" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
