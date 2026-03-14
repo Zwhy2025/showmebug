5509e536816e99b8b4239a77d1a818832900859b
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:28 +0800

    start linked-list-cycle

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 环形链表" << '\n';
+    std::cout << "Slug: linked-list-cycle" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
