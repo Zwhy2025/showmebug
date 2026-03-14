df9025d3540223116cbdb76edd5f25bafe87a12e
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:39 +0800

    start lowest-common-ancestor-of-a-binary-tree

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 二叉树的最近公共祖先" << '\n';
+    std::cout << "Slug: lowest-common-ancestor-of-a-binary-tree" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
