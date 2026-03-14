e242ac292a626887824f742728869913bfcac69a
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:38 +0800

    start flatten-binary-tree-to-linked-list

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 二叉树展开为链表" << '\n';
+    std::cout << "Slug: flatten-binary-tree-to-linked-list" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
