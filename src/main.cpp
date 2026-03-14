1e12ad523f90ad0c85eaf1e7263bb1dde36ddea7
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:26 +0800

    start rotate-image

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 旋转图像" << '\n';
+    std::cout << "Slug: rotate-image" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
