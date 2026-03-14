ac8d833dba25875b28090d5cda7789a4c4c68d13
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:57 +0800

    start unique-paths

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 不同路径" << '\n';
+    std::cout << "Slug: unique-paths" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
