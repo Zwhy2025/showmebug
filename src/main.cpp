20bab5d7919584b752203f7c6b9665b4f5abdf72
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:56 +0800

    start longest-valid-parentheses

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 最长有效括号" << '\n';
+    std::cout << "Slug: longest-valid-parentheses" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
