c292401d643658375f63a1c168d49ae2a2bee273
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:45 +0800

    start find-first-and-last-position-of-element-in-sorted-array

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 在排序数组中查找元素的第一个和最后一个位置" << '\n';
+    std::cout << "Slug: find-first-and-last-position-of-element-in-sorted-array" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
