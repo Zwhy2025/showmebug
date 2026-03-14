de92a040d0d22559c4acfd597daeb243ff0fdc47
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 19:08:55 +0800

    refresh scaffold for find-first-and-last-position-of-element-in-sorted-array

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <optional>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 #include "solution.hpp"
 
+// 在排序数组中查找元素的第一个和最后一个位置
+// https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+template <typename T>
+void checkEqual(const string &caseName, const T &actual, const T &expected) {
+    bool pass = actual == expected;
+    cout << caseName << ": " << (pass ? "PASS" : "FAIL") << '\n';
+}
+
+
 int main() {
-    std::cout << "Current problem: 在排序数组中查找元素的第一个和最后一个位置" << '\n';
-    std::cout << "Slug: find-first-and-last-position-of-element-in-sorted-array" << '\n';
-    std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
+    Solution s;
+
+    {
+        // TODO: replace the placeholders below with a real sample.
+    vector<int> nums{/* TODO */};
+    int target = 0;
+        vector<int> expected{/* TODO */};
+        auto actual = s.searchRange(nums, target);
+        checkEqual("sample-1", actual, expected);
+    }
+
     return 0;
 }
