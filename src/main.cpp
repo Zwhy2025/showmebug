df05c4bf006bc4759a2fc98fa90c00b539d9fff2
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:47 +0800

    start min-stack

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 最小栈" << '\n';
+    std::cout << "Slug: min-stack" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
