8a4553c64becd4f0deeb882ccbab995c58385cbe
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:24 +0800

    start product-of-array-except-self

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 除了自身以外数组的乘积" << '\n';
+    std::cout << "Slug: product-of-array-except-self" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
