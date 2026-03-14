4891d7478646498d1616d7bb377f32d06865cd63
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:42 +0800

    start letter-combinations-of-a-phone-number

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 电话号码的字母组合" << '\n';
+    std::cout << "Slug: letter-combinations-of-a-phone-number" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
