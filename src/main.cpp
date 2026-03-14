7a7fe0d11b39dbbc6f72ee3b473167d6cfaee305
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:53 +0800

    start pascals-triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 杨辉三角" << '\n';
+    std::cout << "Slug: pascals-triangle" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
