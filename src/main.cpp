c3ae40de55e00fcdbfdad979942c2656bb4c5023
zwhy2025 <zwhy2025@gmail.com>
2026-03-14 18:10:25 +0800

    start set-matrix-zeroes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "solution.hpp"
 
 int main() {
-    std::cout << "Current problem: Scratch Pad" << '\n';
-    std::cout << "Slug: scratch" << '\n';
+    std::cout << "Current problem: 矩阵置零" << '\n';
+    std::cout << "Slug: set-matrix-zeroes" << '\n';
     std::cout << "Edit src/solution.hpp and src/main.cpp for local testing." << '\n';
     return 0;
 }
