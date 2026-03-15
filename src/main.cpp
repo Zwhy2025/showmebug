b885954c6142b684119baf11487830b7297b2b08
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:03:43 +0800

    Move problem link to solution

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@ using namespace std;
 
 #include "solution.hpp"
 
-// 二叉树的最近公共祖先
-// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
-
 template <typename T>
 void checkEqual(const string &caseName, const T &actual, const T &expected) {
     bool pass = actual == expected;
--- src/solution.hpp
+++ src/solution.hpp
@@ -23,6 +23,8 @@
 
 using namespace std;
 
+// 二叉树的最近公共祖先
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
 // Definition for a binary tree node.
 struct TreeNode {
     int val;
