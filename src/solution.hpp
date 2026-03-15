f0a922cfa6fb046d2970e59b9e04b6dce520108e
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:57:23 +0800

    codex/hot100/二叉树/简单/104-二叉树的最大深度/20260315-135723

--- src/solution.hpp
+++ src/solution.hpp
@@ -58,4 +58,16 @@ class Solution {
         passTree(root, 1);
         return _vMaxVal;
     }
+
+    /*
+    int maxDepth(TreeNode *root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+        return max(leftDepth, rightDepth) + 1;
+    }
+    */
 };
