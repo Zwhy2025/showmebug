6c2aa5a0ea03f226437cf9da08a8f64b51513bec
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:56:07 +0800

    补全输入和校验逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,28 @@ void checkEqual(const string &caseName, const T &actual, const T &expected) {
     cout << caseName << ": " << (pass ? "PASS" : "FAIL") << '\n';
 }
 
-
 int main() {
     Solution s;
 
     {
-        // TODO: replace the placeholders below with a real sample.
-    vector<int> temperatures{/* TODO */};
-        vector<int> expected{/* TODO */};
-        auto actual = s.dailyTemperatures(temperatures);
+        vector<int> temps{73,74,75,71,69,72,76,73};
+        vector<int> expected{1,1,4,2,1,1,0,0};
+        auto actual = s.dailyTemperatures(temps);
         checkEqual("sample-1", actual, expected);
+    }
+
+    {
+        vector<int> temps{30,40,50,60};
+        vector<int> expected{1,1,1,0};
+        auto actual = s.dailyTemperatures(temps);
+        checkEqual("sample-2", actual, expected);
+    }
+
+    {
+        vector<int> temps{30,60,90};
+        vector<int> expected{1,1,0};
+        auto actual = s.dailyTemperatures(temps);
+        checkEqual("sample-3", actual, expected);
     }
 
     return 0;
