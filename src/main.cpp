5336d3b2fd01b49efdc4795cd48a65d31dcaa00c
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:56:06 +0800

    补全输入和校验逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,22 @@ void checkEqual(const string &caseName, const T &actual, const T &expected) {
     cout << caseName << ": " << (pass ? "PASS" : "FAIL") << '\n';
 }
 
-
 int main() {
     Solution s;
 
     {
-        // TODO: replace the placeholders below with a real sample.
-    vector<vector<int>> grid{/* TODO */};
-        int expected = 0;
-        auto actual = s.orangesRotting(grid);
-        checkEqual("sample-1", actual, expected);
+        vector<vector<int>> grid{{2,1,1},{1,1,0},{0,1,1}};
+        checkEqual("sample-1", s.orangesRotting(grid), 4);
+    }
+
+    {
+        vector<vector<int>> grid{{2,1,1},{0,1,1},{1,0,1}};
+        checkEqual("sample-2", s.orangesRotting(grid), -1);
+    }
+
+    {
+        vector<vector<int>> grid{{0,2}};
+        checkEqual("sample-3", s.orangesRotting(grid), 0);
     }
 
     return 0;
