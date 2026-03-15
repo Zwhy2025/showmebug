93accdb7d3ac86549677d14d22ba28f465af376c
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:03:48 +0800

    Move problem link to solution

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,6 @@ using namespace std;
 
 #include "solution.hpp"
 
-// 回文链表
-// https://leetcode.cn/problems/palindrome-linked-list/
-
 template <typename T>
 void checkEqual(const string &caseName, const T &actual, const T &expected) {
     bool pass = actual == expected;
--- src/solution.hpp
+++ src/solution.hpp
@@ -23,6 +23,8 @@
 
 using namespace std;
 
+// 回文链表
+// https://leetcode.cn/problems/palindrome-linked-list/
 // Definition for singly-linked list.
 struct ListNode {
     int val;
