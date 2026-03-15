32d24e71c227a7282db5bf652cb78c795ecf8f7c
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:56:07 +0800

    补全输入和校验逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,22 @@ void checkEqual(const string &caseName, const T &actual, const T &expected) {
     cout << caseName << ": " << (pass ? "PASS" : "FAIL") << '\n';
 }
 
-
 int main() {
     Solution s;
 
     {
-        // TODO: replace the placeholders below with a real sample.
-    vector<int> nums{/* TODO */};
-        int expected = 0;
-        auto actual = s.findDuplicate(nums);
-        checkEqual("sample-1", actual, expected);
+        vector<int> nums{1,3,4,2,2};
+        checkEqual("sample-1", s.findDuplicate(nums), 2);
+    }
+
+    {
+        vector<int> nums{3,1,3,4,2};
+        checkEqual("sample-2", s.findDuplicate(nums), 3);
+    }
+
+    {
+        vector<int> nums{3,3,3,3,3};
+        checkEqual("sample-3", s.findDuplicate(nums), 3);
     }
 
     return 0;
