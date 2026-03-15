69d54930260a41678156eafe9d87627bc02da3f3
zwhy2025 <zwhy2025@gmail.com>
2026-03-15 13:56:07 +0800

    补全输入和校验逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,24 @@ void checkEqual(const string &caseName, const T &actual, const T &expected) {
     cout << caseName << ": " << (pass ? "PASS" : "FAIL") << '\n';
 }
 
+#include <algorithm>
 
 int main() {
     Solution s;
 
     {
-        // TODO: replace the placeholders below with a real sample.
-    vector<int> nums{/* TODO */};
-    int k = 0;
-        vector<int> expected{/* TODO */};
-        auto actual = s.topKFrequent(nums, k);
+        vector<int> nums{1,1,1,2,2,3};
+        auto actual = s.topKFrequent(nums, 2);
+        sort(actual.begin(), actual.end());
+        vector<int> expected{1,2};
         checkEqual("sample-1", actual, expected);
+    }
+
+    {
+        vector<int> nums{1};
+        auto actual = s.topKFrequent(nums, 1);
+        vector<int> expected{1};
+        checkEqual("sample-2", actual, expected);
     }
 
     return 0;
